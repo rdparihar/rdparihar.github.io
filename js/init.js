bda2564b689bc06faf45d8a449ea5440c4d5f48e
Rajdeep Singh Parihar <rdparihar@yahoo.com>
2015-12-17 18:09:40 +0530

    Update init.js

--- js/init.js
+++ js/init.js
@@ -5,3 +5,18 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+ $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+      
+      // Pause slider
+$('.slider').slider('pause');
+// Start slider
+$('.slider').slider('start');
+// Next slide
+$('.slider').slider('next');
+// Previous slide
+$('.slider').slider('prev');
+      
+      
+    });
+        
