845dd1c4bfa28fd7622997413dd8f3d8d9ba9315
Rajdeep Singh Parihar <rdparihar@yahoo.com>
2015-12-17 18:10:35 +0530

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,15 +8,12 @@
  $(document).ready(function(){
       $('.slider').slider({full_width: true});
       
-      // Pause slider
+      
 $('.slider').slider('pause');
-// Start slider
 $('.slider').slider('start');
-// Next slide
 $('.slider').slider('next');
-// Previous slide
 $('.slider').slider('prev');
-      
-      
-    });
+   
+   
+ });
         
