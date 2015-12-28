fe3963febdaa8ae1984ddf9f6713230e96898d0c
Rajdeep Singh Parihar <rdparihar@yahoo.com>
2015-12-28 19:12:41 +0530

    Update init.js

--- js/init.js
+++ js/init.js
@@ -16,4 +16,9 @@ $('.slider').slider('prev');
    
    
  });
+  $(document).ready(function(){
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+  });
         
