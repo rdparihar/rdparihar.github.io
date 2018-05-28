8a661fd34667085497cd19851f43633367182cc1
rdparihar <rdparihar@yahoo.com>
2018-05-28 15:07:33 +0530

    table

---
+++ snap/myTutorials.js
@@ -0,0 +1,26 @@
+myFunctiong([
+{
+"name": "rdp",
+"url": "https://www.w3schools.com/html/default.asp"
+},
+{
+"name": "rd1",
+"url": "https://www.w3schools.com/css/default.asp"
+},
+{
+"name": "JavaScript Tutorial",
+"url": "https://www.w3schools.com/js/default.asp"
+},
+{
+"name": "SQL Tutorial",
+"url": "https://www.w3schools.com/sql/default.asp"
+},
+{
+"name": "PHP Tutorial",
+"url": "https://www.w3schools.com/php/default.asp"
+},
+{
+"name": "XML Tutorial",
+"url": "https://www.w3schools.com/xml/default.asp"
+}
+])
